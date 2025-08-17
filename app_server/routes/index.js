3465e47989a796b281170f9b782d847be590af51
Srinand Diddi <srinand.com@gmail.com>
2025-08-17 16:39:22 +0530

    Initial commit

---
+++ app_server/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Loc8r' });
+});
+router.get('/location', function(req, res, next)
+ {
+  res.render('index', { title: 'Location info' });
+});
+router.get('/location/review', function(req, res, next)
+ {
+  res.render('index', { title: 'Review' });
+});
+router.get('/about', function(req, res, next)
+ {
+  res.render('index', { title: 'About' });
+});
+
+module.exports = router;
+
